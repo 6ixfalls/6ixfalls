91a63749ce72758d2c79d60df2906c1c193f801e
6ixfalls <bryan888red@gmail.com>
2021-11-14 22:26:18 -0800

    vercel

--- README.md
+++ README.md
@@ -4,6 +4,4 @@
 [![DevForum](https://img.shields.io/badge/dynamic/json?color=9cf&label=DevForum&query=user.profile_view_count&suffix=%20Views&url=https%3A%2F%2Fdevforum.roblox.com%2Fu%2Fsixfalls.json&style=for-the-badge&logo=Roblox)](https://devforum.roblox.com/u/sixfalls/summary)
 [![Discord](https://img.shields.io/badge/Discord-Profile-blue?style=for-the-badge&logo=Discord&logoColor=white)](https://discord.com/users/303173495918034945)
 
-<p align="center">
-  <img align="left" valign="top" src="assets/discord.svg" width="100%" height="200px">
-</p>
+<embed src="assets/banner.svg" width="100%" height="200px">
--- src/api/index.js
+++ src/api/index.js
@@ -1,1 +1,29 @@
+const fetch = require("node-fetch");
+const fs = require("fs");
+const path = require("path");
 
+async function getImageDataURL(url) {
+    let blob = await fetch(url).then(r => r.blob());
+    return new Promise(resolve => {
+        let reader = new FileReader();
+        reader.onload = () => resolve(reader.result);
+        reader.readAsDataURL(blob);
+    });
+};
+
+const urlOne = "https://lanyard-profile-readme.vercel.app/api/303173495918034945?bg=0D1117"
+const urlTwo = "https://github-readme-stats.vercel.app/api?username=6ixfalls&amp;theme=github_dark&amp;hide_border=true&amp;border_radius=10"
+const urlThree = "https://github-readme-stats.vercel.app/api/top-langs/?username=6ixfalls&amp;theme=github_dark&amp;hide_border=true&amp;layout=compact&amp;border_radius=10"
+
+module.exports = async (req, res) => {
+    var banner = fs.readFileSync(path.join(__dirname, "../../assets/banner.svg"), "utf8");
+    var imageDataOne = await getImageDataURL(urlOne);
+    var imageDataTwo = await getImageDataURL(urlTwo);
+    var imageDataThree = await getImageDataURL(urlThree);
+
+    banner.replace("{imageDataOne}", imageDataOne);
+    banner.replace("{imageDataTwo}", imageDataTwo);
+    banner.replace("{imageDataThree}", imageDataThree);
+
+    res.send(banner);
+}
